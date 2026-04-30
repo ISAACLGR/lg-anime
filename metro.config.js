cbbdb8d296e557e943ae9ddabc6a848e8dd59dba
rene-duarte-wmw <rene.duarte@wmw.com.br>
2026-04-29 21:41:55 -0300

    Fix NativeWind Metro config for Vercel

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,8 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
-// Add NativeWind cache to watchFolders
-config.watchFolders = [
-  ...(config.watchFolders || []),
-  path.resolve(__dirname, "node_modules/react-native-css-interop/.cache"),
-];
-
 module.exports = withNativeWind(config, {
   input: "./global.css",
-  forceWriteFileSystem: true,
 });
